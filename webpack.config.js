143741cd446df54eece9d5bf1d3f2b470ae122ed
apple <apple@kel-MacBook.local>
2019-09-14 17:39:52 -0400

    delete public path

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,21 @@
-const path = require ('path') ;
+var path = require ('path') ;
+
 
 module.exports = {
   debug: true,
   devtool: 'inline-source-map',
   noInfo: false,
   entry: [
-    path.resolve(__dirname, 'src/calendar')
+    path.resolve(__dirname, './app.js')
   ],
   target: 'web',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '/',
     filename: 'bundle.js'
+  },
+  node: {
+    __dirname: false,
+    __filename: false
   },
   plugins: [],
   module: {
